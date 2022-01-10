99ed1497c1236a581986997c7600c2a3c7c68839
riabov-vladimir <vriabovsvet@gmail.com>
2022-01-10 20:16:36 +0300

    update settings.py debug

--- testsite1/testsite1/settings.py
+++ testsite1/testsite1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8o+sxk5qnhur07efxejk7$8k%t&#18n-s09j!l-awovzjww1-h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.riabow.com']
 
 
 # Application definition
